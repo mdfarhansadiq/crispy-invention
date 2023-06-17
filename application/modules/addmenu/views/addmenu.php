d407ce55ccb0ddffa258aa1d16c9b5d47182307a
Md. Farhan Sadiq <mdfarhansadiq01@gmail.com>
2023-06-17 22:26:23 +0600

    addmenuslug

--- application/modules/addmenu/views/addmenu.php
+++ application/modules/addmenu/views/addmenu.php
@@ -3,62 +3,85 @@
 
 <head>
     <style>
-    input[type=text],
-    select {
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        display: inline-block;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        box-sizing: border-box;
-    }
+        input[type=text] {
+            width: 70%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
 
-    input[type=submit] {
-        width: 100%;
-        background-color: #4CAF50;
-        color: white;
-        padding: 14px 20px;
-        margin: 8px 0;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-    }
+        input[type=submit] {
+            width: 70%;
+            background-color: #4CAF50;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
 
-    input[type=submit]:hover {
-        background-color: #45a049;
-    }
+        input[type=submit]:hover {
+            background-color: #45a049;
+        }
 
-    div {
-        border-radius: 5px;
-        background-color: #f2f2f2;
-        padding: 20px;
-    }
+        div {
+            border-radius: 5px;
+            background-color: #f2f2f2;
+            padding: 20px;
+        }
     </style>
 </head>
 
 <body>
     <!-- (c) w3schools.com -->
-    <h3>Using CSS to style an HTML Form</h3>
+    <h3 style="text-align: center;">Add Menu - form</h3>
 
     <div>
-        <form action="##">
-            <label for="fname">First Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="Your name..">
+        <form action="<?php echo site_url('addmenu/Addmenu/create'); ?>" method="post">
+            <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
+            <label for="menuname">Menu Name</label>
+            <input type="text" id="menuname" name="menuname" placeholder="Menu Name">
 
-            <label for="lname">Last Name</label>
-            <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+            <div>
+                <h4>Do you want to add Sub-Menu?</h4>
+                <label>Yes</label>
+                <input type="radio" value="Yes" name="opt" id="radioBtnYes" onchange="radioBtnY()">
+                <label>No</label>
+                <input type="radio" value="No" name="opt" id="radioBtnNo" onchange="radioBtnN()">
+            </div>
+            <div style="display: none;" id="menuSlugDiv">
+                <label for="menuslug">Menu Slug</label>
+                <input type="text" id="menuslug" name="menuslug" placeholder="Menu Slug">
+            </div>
 
-            <label for="country">Country</label>
+            <!-- <label for="country">Country</label>
             <select id="country" name="country">
                 <option value="australia">Australia</option>
                 <option value="canada">Canada</option>
                 <option value="usa">USA</option>
-            </select>
+            </select> -->
 
             <input type="submit" value="Submit">
         </form>
     </div>
+
+    <script>
+        function radioBtnY() {
+            var radioBtnValYes = document.getElementById("radioBtnYes").value;
+            document.getElementById("menuSlugDiv").style.display = 'block';
+            console.log(radioBtnValYes);
+        }
+
+        function radioBtnN() {
+            var radioBtnValNo = document.getElementById("radioBtnNo").value;
+            document.getElementById("menuSlugDiv").style.display = 'none';
+            console.log(radioBtnValNo);
+        }
+    </script>
 </body>
 
 </html>
